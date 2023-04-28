467736a95f15f4c4be78c9854934d28fef1f4e47
Carlos && Ponce <srtager555@gmail.com>
2023-04-28 16:40:02 -0600

    basic things from the course added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
+// basic things
+
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const products = [
+	{
+		id: 1,
+		name: "iMac",
+		price: "2400 USD",
+	},
+	{
+		id: 2,
+		name: "iPhone",
+		price: "1000 USD",
+	},
+	{
+		id: 3,
+		name: "AppleWatch",
+		price: "700 USD",
+	},
+	{
+		id: 4,
+		name: "iPad",
+		price: "1200 USD",
+	},
+];
 
 app.get("/", (req, res) => {
 	res.send("Hello");
 });
 
-app.get("/new-route", (req, res) => {
-	res.json({
-		id: 1,
-		name: "iMac",
-		price: "1200 USD",
-	});
+app.get("/products", (req, res) => {
+	res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+	const { id } = req.params;
+	const { color } = req.query;
+
+	if (typeof id === "undefined") {
+		res.json("not found " + id);
+	} else {
+		const product = products.find((el) => el.id === Number(id));
+		if (product) res.json({ product, color });
+		else res.json("not found " + id);
+	}
 });
 
 app.get("/more-routes", (req, res) => {
@@ -19,5 +53,5 @@ app.get("/more-routes", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log("?");
+	console.log(`server ready http://localhost:${port}/`);
 });
