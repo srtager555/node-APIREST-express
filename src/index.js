d4b1473801fd498f70fb68c18a114dee4b5dd604
Carlos && Ponce <srtager555@gmail.com>
2023-04-28 16:25:53 -0600

    express installed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world! Yeah a classic");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+app.get("/new-route", (req, res) => {
+	res.json({
+		id: 1,
+		name: "iMac",
+		price: "1200 USD",
+	});
+});
+
+app.get("/more-routes", (req, res) => {
+	res.send("A new route againt");
+});
+
+app.listen(port, () => {
+	console.log("?");
+});
